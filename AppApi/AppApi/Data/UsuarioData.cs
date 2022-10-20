2e9c7a6dbc498c88e3343ebf6eefe6b7a9aa1f04
EthiemGuerrero <ethiemalex03@gmail.com>
2022-10-19 20:39:53 -0500

    primer proyecto de c# cuarto trimestre

---
+++ AppApi/AppApi/Data/UsuarioData.cs
@@ -0,0 +1,96 @@
+﻿using AppApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace AppApi.Data
+{
+    public class UsuarioData
+    {
+        public static bool registrarUsuario(Usuario oUsuario)
+        {
+            ConexionBD objEst = new ConexionBD();
+            string sentencia;
+            sentencia = "EXECUTE usp_registrar '"+oUsuario.ID_Usuario+"', '"+oUsuario.Nombres+"', '"+oUsuario.Telefono+"', '"+oUsuario.Correo+"', '"+oUsuario.Ciudad+"'";
+
+            if(!objEst.EjecutarSentencia(sentencia, false))
+            {
+                objEst = null;
+                return false;
+            }
+            else
+            {
+                objEst = null;
+                return true;
+            }
+        }
+
+        public static bool actualizarUsuario(Usuario oUsuario)
+        {
+            ConexionBD objEst = new ConexionBD();
+            string sentencia;
+            sentencia = "EXECUTE usp_Actualizar '" + oUsuario.ID_Usuario + "', '" + oUsuario.Nombres + "', '" + oUsuario.Telefono + "', '" + oUsuario.Correo + "', '" + oUsuario.Ciudad + "'";
+
+            if (!objEst.EjecutarSentencia(sentencia, false))
+            {
+                objEst = null;
+                return false;
+            }
+            else
+            {
+                objEst = null;
+                return true;
+            }
+        }
+
+        public static bool eliminarUsuario(Usuario oUsuario)
+        {
+            ConexionBD objEst = new ConexionBD();
+            string sentencia;
+            sentencia = "EXECUTE USP_Elminar'" + oUsuario.ID_Usuario + "'";
+
+            if (!objEst.EjecutarSentencia(sentencia, false))
+            {
+                objEst = null;
+                return false;
+            }
+            else
+            {
+                objEst = null;
+                return true;
+            }
+        }
+
+        public static List<Usuario> Listar()
+        {
+            List<Usuario> oListaUsuario = new List<Usuario>();
+            ConexionBD objEst = new ConexionBD();
+            string sentencia;
+            sentencia = "EXECUTE USP_listar";
+
+            if (objEst.Consultar(sentencia, false))
+            {
+                SqlDataReader dr = objEst.Reader;
+                while (dr.Read())
+                {
+                    oListaUsuario.Add(new Usuario()
+                    {
+                        ID_Usuario = Convert.ToInt32(dr["ID_Usuario"]),
+                        Nombres = dr["Nombres"].ToString(),
+                        Telefono = dr["Telefono"].ToString(),
+                        Correo = dr["Correo"].ToString(),
+                        Ciudad = dr["Ciudad"].ToString(),
+                        FechaIngreso = Convert.ToDateTime(dr["FechaIngreso"].ToString())
+                    });
+                }
+                return oListaUsuario;
+            }
+            else
+            {
+                return oListaUsuario;
+            }
+        }
+    }
+}
---
+++ AppApi/AppApi/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppApi.Models
+{
+    public class Usuario
+    {
+        public int ID_Usuario { get; set; }
+        public string Nombres { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Ciudad { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        
+    }
+}
