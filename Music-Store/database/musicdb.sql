123052434aaef93cadf96adef35758731ab6f840
EthiemGuerrero <ethiemalex03@gmail.com>
2022-10-26 18:53:09 -0500

    base de datos terminada

--- Music-Store/database/musicdb.sql
+++ Music-Store/database/musicdb.sql
@@ -1,10 +1,14 @@
 create database MusicStore;
 use MusicStore;
+drop database MusicStore;
+create database xde;
+use xde;
+
 
 create table Empleado(
 	id_empleado varchar(15) primary key not null,
 	nombre_empleado varchar(20) not null,
-	ocupacion int not null,
+	ocupacion varchar(20) not null,
 	horario_llegada time not null,
 	horario_salida time not null,
 	costo_guitarra money not null,
@@ -16,9 +20,9 @@ create table Empleado(
 );
 go
 create procedure USP_registro 
-	@id_empleado varchar,
-	@nombre_empleado varchar,
-	@ocupacion int,
+	@id_empleado varchar (15),
+	@nombre_empleado varchar (20),
+	@ocupacion varchar (20),
 	@horario_llegada time,
 	@horario_salida time,
 	@costo_guitarra money,
@@ -32,3 +36,70 @@ begin
    insert into Empleado(id_empleado,nombre_empleado,ocupacion,horario_llegada,horario_salida,costo_guitarra,costo_piano,costo_bateria,salario_empleado,compradores_atendidos,fecha_contratacion) values(@id_empleado,@nombre_empleado,@ocupacion,@horario_llegada,@horario_salida, @costo_guitarra,@costo_piano,@costo_bateria,@salario_empleado,@compradores_atendidos,@fecha_contratacion)
 end
 go
+
+execute USP_registro '123456789123456', 'luis sdfghjklñpe', 'sivbnmigtfrdeswaqsdtg', '12:00:00', '06:00:00', 50000, 60000, 70000, 150000, 1800000, '2012-11-24';
+
+go
+
+create procedure USP_borrar
+	@id_empleado varchar(15)
+as
+begin
+	delete from Empleado where id_empleado=@id_empleado;
+end
+go
+
+execute USP_borrar '123456789123456';
+
+go
+create procedure USP_actualizacion 
+	@id_empleado varchar (15),
+	@nombre_empleado varchar (20),
+	@ocupacion varchar (20),
+	@horario_llegada time,
+	@horario_salida time,
+	@costo_guitarra money,
+	@costo_piano money,
+	@costo_bateria money,
+	@salario_empleado float,
+	@compradores_atendidos bigint,
+	@fecha_contratacion date 
+as
+begin
+   update Empleado set
+   nombre_empleado = @nombre_empleado,
+   ocupacion = @ocupacion,
+   horario_llegada =  @horario_llegada,
+   horario_salida = @horario_salida,
+   costo_guitarra = @costo_guitarra,
+   costo_piano = @costo_piano,
+   costo_bateria = @costo_bateria,
+   salario_empleado = @salario_empleado,
+   compradores_atendidos = @compradores_atendidos,
+   fecha_contratacion = @fecha_contratacion
+   where id_empleado = @id_empleado;
+end
+go
+
+execute USP_actualizacion '123456789123456', 'Juan', 'puta', '14:00:00', '08:00:00', 55000, 65000, 75000, 100000, 3800000, '2015-10-22';
+
+go 
+create procedure USP_listarUno
+	@id_empleado varchar(15)
+as
+begin
+	select * from Empleado where id_empleado=@id_empleado;
+end
+go
+
+execute USP_listarUno '123456789123456';
+
+go 
+create procedure USP_listar
+as
+begin 
+	select * from Empleado;
+end
+go
+
+execute USP_listar;
