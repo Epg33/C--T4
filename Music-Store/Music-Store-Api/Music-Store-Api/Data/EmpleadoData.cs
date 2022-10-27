1f5b403e8118e023e40c12593d6df427d60f97d2
EthiemGuerrero <ethiemalex03@gmail.com>
2022-10-26 19:28:06 -0500

    registrar empleado

---
+++ Music-Store/Music-Store-Api/Music-Store-Api/Data/EmpleadoData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Music_Store_Api.Models;
+using System.Data.SqlClient;
+
+namespace Music_Store_Api.Data
+{
+    public class EmpleadoData
+    {
+        public static bool registrarEmpleado(Empleado empleado)
+        {
+            ConexionBD objEst = new ConexionBD();
+            string sentencia;
+            sentencia = "EXECUTE USP_registrar '"+empleado.id_empleado+"' , '"+
+                empleado.nombre_empleado+ "','"+empleado.ocupacion+"', '"+
+                empleado.horario_llegada+"', '"+empleado.horario_salida+"', '"+
+                empleado.costo_guitarra+"', '"+empleado.costo_piano+"', '"+
+                empleado.costo_bateria+"' ,  '"+empleado.salario_empleado+"', '"+
+                empleado.compradores_atendidos+"', '"+empleado.fecha_contratacion+"'";
+            if (!objEst.EjecutarSentencia(sentencia, false))
+            {
+                objEst = null;
+                return false;
+            }
+            else
+            {
+                objEst = null;
+                return true;
+            }
+        }
+
+    }
+}
---
+++ Music-Store/Music-Store-Api/Music-Store-Api/Models/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Music_Store_Api.Models
+{
+    public class Empleado
+    {
+		public string id_empleado { get; set; }
+		public string nombre_empleado { get; set; }
+        public string ocupacion { get; set; }
+        public DateTimeKind horario_llegada { get; set; }
+        public DateTimeKind horario_salida { get; set; }
+        public int costo_guitarra { get; set; }
+        public int costo_piano { get; set; }
+        public int costo_bateria { get; set; }
+        public float salario_empleado { get; set; }
+        public int compradores_atendidos { get; set; }
+        public DateTime fecha_contratacion { get; set; }
+    }
+}
