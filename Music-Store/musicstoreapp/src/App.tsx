539d2f2ac0ab70536e99711ea738d16ef8642d80
EthiemGuerrero <ethiemalex03@gmail.com>
2022-10-29 13:52:12 -0500

    nose

--- Music-Store/musicstoreapp/src/App.tsx
+++ Music-Store/musicstoreapp/src/App.tsx
@@ -3,12 +3,10 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <h1 className="text-red-500">
-      Hello world!
-    </h1>
+   <>
+    
+   </>
   )
 }
 
