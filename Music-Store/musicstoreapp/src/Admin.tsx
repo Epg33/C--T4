32d9b6888121eaed206ebd3cd3a32054dabfd7f8
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-02 15:12:35 -0500

    inicios estilos de la tabla

--- Music-Store/musicstoreapp/src/Admin.tsx
+++ Music-Store/musicstoreapp/src/Admin.tsx
@@ -7,21 +7,23 @@ const Admin = () => {
   })
   return (
     <>
-      <main className="bg-gradient-to-r from-sky-400 to-blue-500 w-screen h-screen">
-        <table className="w-full h-full flex flex-col">
-          <tr className="flex flex-row gap-4">
-            <td className="w-22">ID</td>
-            <td className="w-22">Nombre</td>
-            <td className="w-22">Ocupacion</td>
-            <td className="w-22">Llegada</td>
-            <td className="w-22">Salida</td>
-            <td className="w-22">Guitarras</td>
-            <td className="w-22">Piano</td>
-            <td className="w-22">Bateria</td>
-            <td className="w-22">Salario</td>
-            <td className="w-22">Atendidos</td>
-            <td className="w-22">Fecha Contratacion</td>
+      <main className="bg-gradient-to-r from-sky-400 to-blue-500 w-screen h-screen flex justify-center items-center">
+        <table className="w-4/5 h-4/5 flex flex-col items-centers bg-gray-50 rounded-3xl">
+          <thead className="w-full h-[8%] flex justify-center pt-2 bg-slate-700 rounded-t-3xl">
+          <tr className="flex flex-row gap-4 w-full justify-evenly">
+            <td className="w-22 text-gray-50 font-bold">ID</td>
+            <td className="w-22 text-gray-50 font-bold">Nombre</td>
+            <td className="w-22 text-gray-50 font-bold">Ocupacion</td>
+            <td className="w-22 text-gray-50 font-bold">Llegada</td>
+            <td className="w-22 text-gray-50 font-bold">Salida</td>
+            <td className="w-22 text-gray-50 font-bold">Guitarras</td>
+            <td className="w-22 text-gray-50 font-bold">Piano</td>
+            <td className="w-22 text-gray-50 font-bold">Bateria</td>
+            <td className="w-22 text-gray-50 font-bold">Salario</td>
+            <td className="w-22 text-gray-50 font-bold">Atendidos</td>
+            <td className="w-22 text-gray-50 font-bold">Fecha Contratacion</td>
           </tr>
+          </thead>
         </table>
       </main>
     </>
