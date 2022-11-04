759a239dcfd97479cbbfb1bef4613afebe0298a8
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-04 11:36:32 -0500

    corregir renderizar empleados

--- Music-Store/musicstoreapp/src/Admin.tsx
+++ Music-Store/musicstoreapp/src/Admin.tsx
@@ -1,10 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Admin = () => {
+  const [table, setTable] = useState<JSX.Element[]>([])
+
+  const tableContent = (prop:any[]) => {
+    setTable(
+      prop.map(emp => {
+        return <tr key={emp.id_empleado}>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.id_empleado}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.nombre_empleado}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.cupacion}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.horario_llegada}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.horario_salida}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.costo_guitarra}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.costo_piano}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.costo_bateria}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.salario_empleado}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.compradores_atendidos}</td>
+          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">{emp.fecha_contratacion}</td>
+        </tr>
+      })
+    )
+  }
+
   useEffect(():void =>{
-    axios.get('http://www.musicstoreapii.somee.com/api/empleado').then(res=>console.log(res))
-  })
+    axios.get('http://www.musicstoreapii.somee.com/api/empleado').then(res=>{tableContent(res.data); console.log(res)})
+  }, [])
   return (
     <>
       <main className="bg-gradient-to-r from-sky-400 to-blue-500 w-screen h-screen flex justify-center items-center">
@@ -24,20 +46,8 @@ const Admin = () => {
             <td className="w-20 text-gray-50 text-sm font-bold ">Contratacion</td>
           </tr>
           </thead>
-          <tbody className="w-full h-[8%] flex justify-center pt-2 rounded-t-3xl pl-0">
-            <tr className="flex flex-row gap-4 w-full justify-around px-5">
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-              <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap">contenido prueba</td>
-            </tr>
+          <tbody className="w-full h-[8%] flex flex-col items-center pt-2 rounded-t-3xl pl-0">
+            {table}
           </tbody>
         </table>
       </main>
