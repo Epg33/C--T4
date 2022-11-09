79e1dfab066a5febdbda46b6dbd9ab88a42af0f6
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-08 19:45:50 -0500

    glass

--- Music-Store/musicstoreapp/src/Admin.tsx
+++ Music-Store/musicstoreapp/src/Admin.tsx
@@ -11,17 +11,17 @@ const Admin = () => {
     setTable(
       prop.map(emp => {
         return <tr className="flex flex-row w-full justify-around px-5" key={emp.id_empleado}>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.id_empleado}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.nombre_empleado}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.ocupacion}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.horario_llegada}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.horario_salida}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.costo_guitarra}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.costo_piano}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.costo_bateria}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/elmpleado/${emp.id_empleado}`}>{emp.salario_empleado}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.compradores_atendidos}</Link></td>
-          <td className="w-20 text-slate-700 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.fecha_contratacion}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.id_empleado}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.nombre_empleado}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.ocupacion}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.horario_llegada}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.horario_salida}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.costo_guitarra}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.costo_piano}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.costo_bateria}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/elmpleado/${emp.id_empleado}`}>{emp.salario_empleado}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.compradores_atendidos}</Link></td>
+          <td className="w-20 text-slate-100 text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"><Link to={`/empleado/${emp.id_empleado}`}>{emp.fecha_contratacion}</Link></td>
         </tr>
       })
     )
@@ -32,8 +32,8 @@ const Admin = () => {
   }, [])
   return (
     <>
-      <main className="bg-gradient-to-r from-sky-400 to-blue-500 w-screen h-screen flex flex-col justify-center items-center">
-        <table className="w-[93%] h-4/5 flex flex-col items-centers bg-gray-50 rounded-3xl shadow-xl mb-14">
+      <main className="bg-gradient-to-r from-sky-400 to-blue-800 w-screen h-screen flex flex-col justify-center items-center">
+        <table className="w-[93%] h-4/5 flex flex-col items-centers rounded-3xl shadow-xl mb-14">
           <thead className="w-full h-[8%] flex pt-4 bg-slate-700 rounded-t-3xl">
           <tr className="flex flex-row w-full justify-around px-5">
             <td className="w-20 text-gray-50 text-sm font-bold ">ID</td>
@@ -49,11 +49,11 @@ const Admin = () => {
             <td className="w-20 text-gray-50 text-sm font-bold ">Contratacion</td>
           </tr>
           </thead>
-          <tbody className="w-full h-[8%] flex flex-col items-center pt-2 rounded-t-3xl pl-0">
+          <tbody className=" shadow-2xl rounded-3xl w-full h-full flex flex-col items-center pt-2 rounded-t-3xl pl-0 ">
             {table}
           </tbody>
         </table>
-        <NavLink to='/registro' className="grid place-content-center w-20 h-10 rounded-md bg-white transition-all duration-200 ease-linear hover:bg-yellow-200 hover:rounded-full hover:h-16 active:translate-y-2"><AiOutlineUserAdd className="text-3xl"/></NavLink>
+        <NavLink to='/registro' className=" grid place-content-center w-20 h-10 rounded-md bg-white transition-all duration-200 ease-linear hover:bg-yellow-200 hover:rounded-full hover:h-16 active:translate-y-2"><AiOutlineUserAdd className="text-3xl"/></NavLink>
       </main>
     </>
 
