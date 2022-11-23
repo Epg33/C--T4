51c49c5415c7d03546ac1e30002a4f9fc2c52ae5
EthiemGuerrero <ethiemalex03@gmail.com>
2022-11-22 19:49:25 -0500

    szs

---
+++ api-net-core/api/ApiAsp/ApiAsp/Controllers/ProductoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using System.Data;
+using System.Data.SqlClient;
+using ApiAsp.Modelos;
+
+
+namespace ApiAsp.Controllers
+{
+    [EnableCors("ReglasCors")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly string cadenaSQL;
+        public ProductoController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("cadenaSQL");
+        }
+
+        [HttpGet]
+        [Route("Lista")] 
+        public IActionResult Lista()
+        {
+            List<Producto> lista = new List<Producto>();
+
+
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("USP_Listar_Producto", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            lista.Add(new Producto
+                            {
+                                IdProducto = Convert.ToInt32(rd["IdProducto"]),
+                                CodigoBarra = rd["CodigoBarra"].ToString(),
+                                Nombre = rd["Nombre"].ToString(),
+                                Marca = rd["Marca"].ToString(),
+                                Categoria = rd["Categoria"].ToString(),
+                                Precio = Convert.ToDecimal(rd["Precio"])
+                            });
+                        }
+                    }
+                }
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = lista });
+            }
+            catch(Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message, response = lista });
+            }
+        }
+        
+    }
+}
